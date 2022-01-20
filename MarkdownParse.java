4e3f8c5f029da1225e3db011173264610881ab6d
sofie <sofie.e.gmerek@gmail.com>
2022-01-20 14:04:06 -0800

    tests

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,13 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            currentIndex = closeParen + 1; 
         }
         return toReturn;
     }
